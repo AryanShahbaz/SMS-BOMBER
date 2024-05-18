0f02813ed4e423e1f18e51370b131971f86b049c
Aryan Shahbaz <aryanshahbaz86@gmail.com>
2024-05-18 22:41:56 +0330

    End 2

--- end.py
+++ end.py
@@ -1,2 +1,8 @@
+import pyfiglet
+from colorama import Fore,init
+init(autoreset=True)
+
 def bye_text():
-       print("Goodbye")
+       fig=pyfiglet.Figlet(font='slant')
+       ascii_art = fig.renderText("Good Bye")
+       print(Fore.LIGHTYELLOW_EX + ascii_art)
