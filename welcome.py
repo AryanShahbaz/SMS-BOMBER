5a64b526a096f82752ec0c860dad2920c016e909
Aryan Shahbaz <aryanshahbaz86@gmail.com>
2024-05-18 22:35:48 +0330

    Welcome 2

--- welcome.py
+++ welcome.py
@@ -1,2 +1,8 @@
+import pyfiglet
+from colorama import Fore,init
+init(autoreset=True)
+
 def welcome_text() :
-       print("WELCOME")
+       text=pyfiglet.figlet_format("SmS BoooMBER",font="slant")
+       print(Fore.RED + text)
+       print(Fore.BLUE + "\t\t\t\tBy:Aryan")
