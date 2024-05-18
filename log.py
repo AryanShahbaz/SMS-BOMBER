b0c60c8eddfd6b23bd90b8b608b6609ce0644c5d
Aryan Shahbaz <aryanshahbaz86@gmail.com>
2024-05-18 22:29:02 +0330

    Log

--- log.py
+++ log.py
@@ -4,4 +4,3 @@ init(autoreset=True)
 def log(number,url):
        print(Fore.YELLOW + number + Fore.RED + " : " + Fore.GREEN + "Successfully" + Fore.RED + " -> " + Fore.LIGHTBLUE_EX + url)
 
-log("11228","gay")
