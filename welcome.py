f1cdd0d8a210277d34612a93589af261c6c7034f
Aryan Shahbaz <aryanshahbaz86@gmail.com>
2024-12-17 08:13:18 +0330

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -5,4 +5,4 @@ init(autoreset=True)
 def welcome_text() :
        text=pyfiglet.figlet_format("SmS BoooMBER",font="slant")
        print(Fore.RED + text)
-       print(Fore.BLUE + "\t\t\t\tBy:Aryan")
+       
